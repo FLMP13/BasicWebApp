18fee161adb44f32ea12a6dabf5ebf25f7180fe6
Yassine_Sfar <yassinesfar7@gmail.com>
2022-07-13 19:50:58 +0200

    n

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,20 +2,36 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
-        } else { // TODO extend the programm here
+            return "MyTeam";
+        } else if (query.contains("which of the following numbers is the largest")) {
+            String[] table = new String[Integer.MAX_VALUE];
+            table = query.split("%");
 
-            return "flmp13";
+            List<Integer> numbers = new ArrayList<>();
+
+            for (String e : table) {
+                try{
+                    numbers.add(Integer.parseInt(e));
+                } catch (NumberFormatException ignored){
+
+                }
+
+            }
+            return String.valueOf(numbers.stream().max(Integer::compare));
         }
+        return "";
     }
 }
